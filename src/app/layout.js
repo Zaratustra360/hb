7b4a8b7dbc51e1012b6aaebaa644cfb3da772903
Lev <zaratustra360@gmail.com>
2023-05-13 20:22:27 +0300

    Second commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.sass'
+import { Alegreya_Sans } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const ASans = Alegreya_Sans({
+  weight: ['300','400', '500', '700'],
+  subsets: ['latin']
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={ASans.className}>{children}</body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,107 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.sass";
+import Link from "next/link";
+import logo from "/public/img/logo.png";
+import barbaraHi from "/public/img/Barbarahi.png";
+import lesson from "/public/img/lesson.jpg";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <header>
+        <div className="container">
+          <nav>
+            <div className="left">
+              <Link href="#">
+                <Image className="logo" src={logo} alt={"logo"} />
+              </Link>
+              <Link href="#">
+                <h2>HappyBara</h2>
+              </Link>
+            </div>
+            <Link
+              href="https://ru.myhappybara.com/training/view/metodika-myhappybara-dlya-prepodavateley-angliyskogo-detyam-level-1/lesson/lesson-1-zoo"
+              className="button"
+              target="_blank"
+            >
+              Перейти к программе
+            </Link>
+          </nav>
         </div>
-      </div>
+      </header>
+      <section className="opening">
+        <div className="container">
+          <h1>
+            Учебная программа английского языка для школ с уникальной методикой
+            обучения языку
+          </h1>
+          <p>Что мы предлагаем?</p>
+          <section>
+            <div className="left">
+              <ul>
+                1. Полностью готовую и эффективную учебную программау, состоящую
+                из:
+                <li>
+                  планов уроков, видеоматериалов с инструкциями для
+                  преподавателей на каждое занятие.
+                </li>
+                <li>
+                  видеоматериалов ученикам для закрепления знаний на дому.
+                </li>
+              </ul>
+              <ul>
+                2. Привлечение новых клиентов за счет включения в список
+                сертифицированных преподавателей, продвижения через программу и
+                соцсети преподавателей.
+              </ul>
+              <ul>
+                {" "}
+                3. Компенсацию расходов на программу и обеспечиваем
+                дополнительный доход от лицензионных платежей клиентов и
+                партнеров:
+                <li>
+                  потенциальный прямой доход партнера с 60 учениками за 1 год -
+                  от 60+ тыс. руб.
+                </li>
+              </ul>
+            </div>
+            <div className="right">
+              <Image src={barbaraHi} alt="barbara"></Image>
+            </div>
+          </section>
+        </div>
+      </section>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="lesson">
+        <div className="container">
+          <h2>Пример урока </h2>
+          <div className="box">
+            <div className="left">
+              <Image className="lesson__img" src={lesson} alt="lesson"></Image>
+            </div>
+            <div className="right">
+              <h3>Модуль 1:</h3>
+              <ul>
+                <li>Тема: зоопарк</li>
+                <li>Кол-во уроков: 8</li>
+                <li>Кол-во планов: 8</li>
+                <li>Кол-во роликов: 39</li>
+              </ul>
+            </div>
+          </div>
+          <Link
+            className="button"
+            target="_blank"
+            href="https://ru.myhappybara.com/training/view/metodika-myhappybara-dlya-prepodavateley-angliyskogo-detyam-level-1/lesson/lesson-1-zoo"
+          >
+            Перейти
+          </Link>
+        </div>
+      </section>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="author">
+        <div className="container"></div>
+      </section>
     </main>
-  )
+  );
 }
