1211d48c02626b6640ef70ef4ed68040c0774abd
Lev <zaratustra360@gmail.com>
2023-05-13 23:06:11 +0300

    4 commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,104 +4,143 @@ import Link from "next/link";
 import logo from "/public/img/logo.png";
 import barbaraHi from "/public/img/Barbarahi.png";
 import lesson from "/public/img/lesson.jpg";
+import author from "/public/img/author.jpg";
 
 export default function Home() {
   return (
-    <main>
-      <header>
-        <div className="container">
-          <nav>
-            <div className="left">
-              <Link href="#">
-                <Image className="logo" src={logo} alt={"logo"} />
+    <>
+      <main>
+        <header>
+          <div className="container">
+            <nav>
+              <div className="left">
+                <Link href="#">
+                  <Image className="logo" src={logo} alt={"logo"} />
+                </Link>
+                <Link href="#">
+                  <h2>HappyBara</h2>
+                </Link>
+              </div>
+              <Link
+                href="https://ru.myhappybara.com/training/view/metodika-myhappybara-dlya-prepodavateley-angliyskogo-detyam-level-1/lesson/lesson-1-zoo"
+                className="button"
+                target="_blank"
+              >
+                Перейти к программе
               </Link>
-              <Link href="#">
-                <h2>HappyBara</h2>
-              </Link>
+            </nav>
+          </div>
+        </header>
+        <section className="opening">
+          <div className="container">
+            <h1>
+              Учебная программа английского языка для школ с уникальной
+              методикой обучения языку
+            </h1>
+            <p>Что мы предлагаем?</p>
+            <section>
+              <div className="left">
+                <ul>
+                  1. Полностью готовую и эффективную учебную программау,
+                  состоящую из:
+                  <li>
+                    планов уроков, видеоматериалов с инструкциями для
+                    преподавателей на каждое занятие.
+                  </li>
+                  <li>
+                    видеоматериалов ученикам для закрепления знаний на дому.
+                  </li>
+                </ul>
+                <ul>
+                  2. Привлечение новых клиентов за счет включения в список
+                  сертифицированных преподавателей, продвижения через программу
+                  и соцсети преподавателей.
+                </ul>
+                <ul>
+                  {" "}
+                  3. Компенсацию расходов на программу и обеспечиваем
+                  дополнительный доход от лицензионных платежей клиентов и
+                  партнеров:
+                  <li>
+                    потенциальный прямой доход партнера с 60 учениками за 1 год
+                    - от 60+ тыс. руб.
+                  </li>
+                </ul>
+              </div>
+              <div className="right">
+                <Image src={barbaraHi} alt="barbara"></Image>
+              </div>
+            </section>
+          </div>
+        </section>
+
+        <section className="lesson">
+          <div className="container">
+            <h2>Пример урока </h2>
+            <div className="box">
+              <div className="left">
+                <Image
+                  className="lesson__img"
+                  src={lesson}
+                  alt="lesson"
+                ></Image>
+              </div>
+              <div className="right">
+                <h3>Модуль 1:</h3>
+                <ul>
+                  <li>Тема: зоопарк</li>
+                  <li>Кол-во уроков: 8</li>
+                  <li>Кол-во планов: 8</li>
+                  <li>Кол-во роликов: 39</li>
+                </ul>
+              </div>
             </div>
             <Link
-              href="https://ru.myhappybara.com/training/view/metodika-myhappybara-dlya-prepodavateley-angliyskogo-detyam-level-1/lesson/lesson-1-zoo"
               className="button"
               target="_blank"
+              href="https://ru.myhappybara.com/training/view/metodika-myhappybara-dlya-prepodavateley-angliyskogo-detyam-level-1/lesson/lesson-1-zoo"
             >
-              Перейти к программе
+              Перейти
             </Link>
-          </nav>
-        </div>
-      </header>
-      <section className="opening">
+          </div>
+        </section>
+      </main>
+      <section className="author">
         <div className="container">
-          <h1>
-            Учебная программа английского языка для школ с уникальной методикой
-            обучения языку
-          </h1>
-          <p>Что мы предлагаем?</p>
-          <section>
-            <div className="left">
-              <ul>
-                1. Полностью готовую и эффективную учебную программау, состоящую
-                из:
-                <li>
-                  планов уроков, видеоматериалов с инструкциями для
-                  преподавателей на каждое занятие.
-                </li>
-                <li>
-                  видеоматериалов ученикам для закрепления знаний на дому.
-                </li>
-              </ul>
-              <ul>
-                2. Привлечение новых клиентов за счет включения в список
-                сертифицированных преподавателей, продвижения через программу и
-                соцсети преподавателей.
-              </ul>
-              <ul>
-                {" "}
-                3. Компенсацию расходов на программу и обеспечиваем
-                дополнительный доход от лицензионных платежей клиентов и
-                партнеров:
-                <li>
-                  потенциальный прямой доход партнера с 60 учениками за 1 год -
-                  от 60+ тыс. руб.
-                </li>
-              </ul>
-            </div>
-            <div className="right">
-              <Image src={barbaraHi} alt="barbara"></Image>
-            </div>
-          </section>
-        </div>
-      </section>
-
-      <section className="lesson">
-        <div className="container">
-          <h2>Пример урока </h2>
+          <h2>Автор</h2>
           <div className="box">
             <div className="left">
-              <Image className="lesson__img" src={lesson} alt="lesson"></Image>
+              <Image className="author__img" src={author} alt="author" />
+              <div className="border"></div>
             </div>
             <div className="right">
-              <h3>Модуль 1:</h3>
-              <ul>
-                <li>Тема: зоопарк</li>
-                <li>Кол-во уроков: 8</li>
-                <li>Кол-во планов: 8</li>
-                <li>Кол-во роликов: 39</li>
-              </ul>
+              <div className="name">Александра Галка</div>
+              <div className="line"></div>
+              <div className="desk">
+                Руководитель учебного центра “Happy Nation”, педагог с 20-ти
+                летним стажем и автор уникальной программы обучения детей
+                английскому языку без опоры на родной язык.
+              </div>
             </div>
           </div>
-          <Link
-            className="button"
-            target="_blank"
-            href="https://ru.myhappybara.com/training/view/metodika-myhappybara-dlya-prepodavateley-angliyskogo-detyam-level-1/lesson/lesson-1-zoo"
-          >
-            Перейти
-          </Link>
         </div>
       </section>
 
-      <section className="author">
-        <div className="container"></div>
+      <section className="method">
+        <div className="container">
+          <h2>О методике</h2>
+          <p className="desk">
+            Программа основа на беспереводной методике обучения. Вы убедитесь,
+            что язык можно осваивать естественным образом как родной, не
+            прибегая к переводу на русский язык и не формируя языковой барьер.
+            Методика “HappyBara” кардинально отличается от привычной школьной
+            «скажи-переведи». Материал вводится постепенно от простого к
+            сложному. Ребенку будет все понятно с первых минут занятия! Дети
+            начинают мыслить и говорить на языке, а не просто запоминают слова
+            врассыпную, которые потом не могут составить в предложение.
+          </p>
+        </div>
       </section>
-    </main>
+    </>
   );
 }
